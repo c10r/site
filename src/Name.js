3d89e5ef8036f869db27a70baf20e9c457f867fc
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 01:07:05 -0700

    Add custom font to main name

--- src/Name.js
+++ src/Name.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Name = (props) => {
   return (
-    <div className="self-center flex text-6xl w-screen justify-center">
+    <div className="font-bold flex-grow self-center flex justify-center"
+      style={{ fontFamily: 'poppins', fontSize: '4.1rem', letterSpacing: '1px' }}>
       <p className="flex-none text-red-700 pr-3">Chander</p>
       <p className="flex-none text-gray-100">Ramesh</p>
     </div>
