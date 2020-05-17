89320628a56e344ddf64fdbb69caf0ee493e446c
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 00:14:42 -0700

    Add missing semicolons in Name.js

--- src/Name.js
+++ src/Name.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Name = (props) => {
   return (
@@ -9,4 +9,4 @@ const Name = (props) => {
   );
 }
 
-export default Name
+export default Name;
