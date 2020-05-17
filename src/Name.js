e7c96038f17b59890ee9cae4efc11d0a10533430
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 01:21:04 -0700

    Run auto formatter in VSCode

--- src/Name.js
+++ src/Name.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Name = (props) => {
   return (
-    <div className="font-bold flex-grow self-center flex justify-center"
-      style={{ fontFamily: 'poppins', fontSize: '4.1rem', letterSpacing: '1px' }}>
+    <div
+      className="font-bold flex-grow self-center flex justify-center"
+      style={{
+        fontFamily: "poppins",
+        fontSize: "4.1rem",
+        letterSpacing: "1px",
+      }}
+    >
       <p className="flex-none text-red-700 pr-3">Chander</p>
       <p className="flex-none text-gray-100">Ramesh</p>
     </div>
   );
-}
+};
 
 export default Name;
