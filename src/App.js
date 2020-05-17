05bea033c1d6374046ce27cd1d0464ebcb17c5f4
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 01:21:20 -0700

    Add social to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Name from './Name.js';
+import Social from './Social.js';
 
 function App() {
   return (
     <div className="flex w-screen h-screen">
+      <Social />
       <Name />
     </div>
   );
