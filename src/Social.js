38800c519d9738b9f35bf4e38712cfa2f0ef5a89
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 12:15:09 -0700

    Prevent particlesJS from swallowing click / hover of social buttons

--- src/Social.js
+++ src/Social.js
@@ -7,18 +7,19 @@ import { ReactComponent as CodingPortfolio } from "./assets/coding.svg";
 
 const animation = `transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:opacity-100`;
 const image = `h-8 w-8 opacity-50 ml-8`;
+const style = { cursor: "pointer", zIndex: "2", fill: "white" }
 
 const Social = (props) => {
   return (
     <div className="w-1/3 flex-none flex flex-col h-screen justify-center">
       <Twitter
         className={`${image} ${animation} mb-4`}
-        style={{ cursor: "pointer" }}
+        style={style}
         onClick={() => (window.location.href = "https://twitter.com/chander")}
       />
       <LinkedIn
         className={`${image} ${animation} mb-4`}
-        style={{ fill: "white", cursor: "pointer" }}
+        style={style}
         onClick={() =>
           (window.location.href =
             "https://www.linkedin.com/in/chandrasekarramesh/")
@@ -26,12 +27,12 @@ const Social = (props) => {
       />
       <Quill
         className={`${image} ${animation} mb-4`}
-        style={{ fill: "white", cursor: "pointer" }}
+        style={style}
         onClick={() => (window.location.href = "https://blog.chander.app")}
       />
       <CodingPortfolio
         className={`${image} ${animation}`}
-        style={{ fill: "white", cursor: "pointer" }}
+        style={style}
       />
     </div>
   );
