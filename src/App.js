cf4efeb337ac033bc4727e637b6fbe28fdd12a55
Chander Ramesh <hardly.hacking@gmail.com>
2020-05-17 10:27:14 -0700

    Add profile image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Name from './Name.js';
-import Social from './Social.js';
+import React from "react";
+import Name from "./Name.js";
+import Social from "./Social.js";
+import Profile from "./Profile.js";
 
 function App() {
   return (
     <div className="flex w-screen h-screen">
       <Social />
       <Name />
+      <Profile />
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import picture from "./assets/profile.png";
+
+const Profile = (props) => {
+  return (
+    <img
+      className="self-center opacity-50"
+      src={picture}
+      style={{
+        WebkitMaskImage:
+          "-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))",
+        maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
+      }}
+    />
+  );
+};
+
+export default Profile;
